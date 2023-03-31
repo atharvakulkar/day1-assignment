ca40ccf1d00efc3d80f0eec1d626122cf0ef3f55
atharvakulkar <123885535+atharvakulkar@users.noreply.github.com>
2023-03-31 14:49:20 +0530

    Add files via upload

---
+++ day1 assignment.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().set_next_input('1) Who developed python programming language');get_ipython().run_line_magic('pinfo', 'language')
+Guido Van Rossum
+
+
+# In[2]:
+
+
+get_ipython().set_next_input('2) which type of progrmaming does python support');get_ipython().run_line_magic('pinfo', 'support')
+Object oriented programing and structured programing
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('3) is python case sensitive when dealing with identifier');get_ipython().run_line_magic('pinfo', 'identifier')
+yes it is a case sensitive
+
+
+
+get_ipython().set_next_input('4)what is the correct extension of python file');get_ipython().run_line_magic('pinfo', 'file')
+py
+
+
+5)is python code compiled or interpreted ?
+Python is an interpreted language, which means the source code of a Python program is converted into bytecode
+that is then executed by the Python virtual machine
+
+get_ipython().set_next_input('6)Name a few blocks of code used to define in Python language');get_ipython().run_line_magic('pinfo', 'language')
+Python uses indentation to define blocks of code. Indentations are simply spaces or tabs used as an
+indicator that is part of the indent code child.
+
+if (condition):
+
+# Statement 1
+
+else:
+
+# Statement 2
+
+You will find that statements 1 will have the same indentation and therefore is of the same block. And similarly, 
+statements 2 is of a another block.
+
+get_ipython().set_next_input('State a character used to give single-line comments in Python');get_ipython().run_line_magic('pinfo', 'Python')
+' # ' symbol to write a single-line comment.
+
+Mention functions which can help us to find the version of python that
+
+
+
+
+get_ipython().set_next_input('7)State a character used to give single-line comments in Python');get_ipython().run_line_magic('pinfo', 'Python')
+' # ' symbol to write a single-line comment.
+
+get_ipython().set_next_input('8) Mention functions which can help us to find the version of python that we are currently working on');get_ipython().run_line_magic('pinfo', 'on')
+The function sys.version can help us to find the version of python that we are currently working on.
+
+9) Python supports the creation of anonymous functions at runtime, using a construct called
+Lambda
+
+get_ipython().set_next_input('10) What does pip stand for python');get_ipython().run_line_magic('pinfo', 'python')
+Preferred Installer Program
+
+get_ipython().set_next_input('11)Mention a few built- in functions in python');get_ipython().run_line_magic('pinfo', 'python')
+bool()
+
+get_ipython().set_next_input('12)What is the maximum possible length of an identifier in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+79 characters
+
+get_ipython().set_next_input('13)What are the benefits of using Python');get_ipython().run_line_magic('pinfo', 'Python')
+Simple and Easy to learn
+Portable and Extensible
+Object-Oriented Programming
+Artificial Intelligence
+Big Data
+Python can handle plenty of an immense amount of data.
+
+get_ipython().set_next_input('14)How is memory managed in Python');get_ipython().run_line_magic('pinfo', 'Python')
+Memory in Python is managed by Python private heap space. All Python objects and data structures are located in a private heap.
+This private heap is taken care of by Python Interpreter itself, and a programmer doesn’t have access to this private heap.
+Python memory manager takes care of the allocation of Python private heap space. Memory for Python private heap space is made
+available by Python’s in-built garbage collector, which recycles and frees up all the unused memory. The allocation of heap 
+space for Python objects and other internal buffers is performed on demand by the Python memory manager through the Python/C
+API functions listed in this document. To avoid memory corruption, extension writers should never try to operate on Python 
+objects with the functions exported by the C library: malloc(), calloc(), realloc() and free().
+
+get_ipython().set_next_input('15)How to install Python on Windows and set path variables');get_ipython().run_line_magic('pinfo', 'variables')
+First, we need to locate where the python is being installed after downloading it. Press WINDOWS key and search for “Python”,
+you will get something like this:Click on open file location and you will be in a location where Python is installed, Copy the
+    location path from the top by clicking over it. Now, we have to add the above-copied path as a variable so that windows
+    can recognize. Search for “Environmental Variables”, you will see something like this: Click on that( Now click the
+                                                                                                         “Environmental
+                                                                                                         Variables” 
+button
+                                                                                                         
+           get_ipython().set_next_input('           16)is indentation is required for the python');get_ipython().run_line_magic('pinfo', 'python')
+Indentation refers to the spaces at the beginning of a code line. Where in other programming languages the indentation 
+in code is for readability only, for Python is very important. Python uses indentation to indicate
+a block of code
+
+ 
+
